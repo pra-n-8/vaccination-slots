13164d93bef607ad15dc3097ed6eb39a53a3a561
pra-n-8 <32676696+pra-n-8@users.noreply.github.com>
2021-05-24 19:21:21 +0530

    sdsd

--- getslots.py
+++ getslots.py
@@ -1,13 +1,14 @@
 from datetime import date
 import requests
 import time
-from flask import Flask, jsonify
+from flask import jsonify
 
 def main():
     i=0
     while(True):
         t=date.today()
         from_date = t.strftime("%d-%m-%Y")
+        print(from_date)
         headers={"user-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"}
         disricts=[359,392]
         for district in disricts:
@@ -22,7 +23,7 @@ def checkSlots(centers):
         # print (c["sessions"])
         for s in center["sessions"]:
         # print (s["available_capacity_dose2"])
-            if(s["available_capacity_dose1"]>0):
+            if(s["available_capacity_dose1"]>=1):
             # print(center)
                 m0="**** "+s["vaccine"]+" **** "
                 m1=str(s["available_capacity_dose1"])+" Vaccine Slot Available at Center "+center["name"]+", "+center["block_name"]+", "+center["district_name"]+", Dose 1 for Age "+ str(s["min_age_limit"])+" & above"
@@ -30,17 +31,13 @@ def checkSlots(centers):
             # print(message)
                 requests.post("https://api.telegram.org/bot1812087446:AAGMKkvTmyCIIcTfdFVEu1iKJA0R5zCeaeI/sendMessage?chat_id=-560862782&text="+m0+m2+m1)
 
-            if(s["available_capacity_dose2"]>0):
+            if(s["available_capacity_dose2"]>=1):
             # print(center)
                 m0="**** "+s["vaccine"]+" **** "
                 m1=str(s["available_capacity_dose1"])+" Vaccine Slot Available at Center "+center["name"]+", "+center["block_name"]+", "+center["district_name"]+", Dose 1 for Age "+ str(s["min_age_limit"])+" & above"
                 m2="|| Date "+s["date"] +"|| *** DOSE 2*** || "
             # print(message)
                 requests.post("https://api.telegram.org/bot1812087446:AAGMKkvTmyCIIcTfdFVEu1iKJA0R5zCeaeI/sendMessage?chat_id=-560862782&text="+m0+m2+m1)
-                
-                
-app = Flask(__name__)
-
 
 if __name__ == "__main__":
     main()
