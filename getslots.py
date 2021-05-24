fad148a53394af7e7e50d517de93cc62d59413b1
pra-n-8 <32676696+pra-n-8@users.noreply.github.com>
2021-05-24 17:55:48 +0530

    Add files via upload

--- getslots.py
+++ getslots.py
@@ -4,16 +4,14 @@ import time
 def main():
     # print("called")
 	i=0
-    while (i<10):
-        today=date.today()
+    while(i<10):
+		today=date.today()
         from_date=today.strftime("%d-%m-%Y")
-#         print(from_date)
         headers={"user-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"}
         districts=[395,392]
         for district in districts:
             response = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id="+str(district)+"&date="+from_date,headers=headers)
             Vcenters = response.json()
-#             print(district)
             checkSlots(Vcenters)
 		i+=1
         #time.sleep(60000)
@@ -30,7 +28,7 @@ def checkSlots(centers):
                 m2="|| Date "+s["date"] +"|| *** DOSE 1*** || "
             # print(message)
                 requests.post("https://api.telegram.org/bot1812087446:AAGMKkvTmyCIIcTfdFVEu1iKJA0R5zCeaeI/sendMessage?chat_id=-560862782&text="+m0+m2+m1)
-			
+
             if(s["available_capacity_dose2"]>=0):
             # print(center)
                 m0="**** "+s["vaccine"]+" **** "
