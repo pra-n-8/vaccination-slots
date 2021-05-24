2cec883726f6fde065c41d8da616f5d3f90446ee
pra-n-8 <32676696+pra-n-8@users.noreply.github.com>
2021-05-24 18:59:25 +0530

    asd

--- getslots.py
+++ getslots.py
@@ -13,7 +13,7 @@ def main():
         for district in disricts:
             print(district)
             response = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id="+str(district)+"&date="+from_date,headers=headers)
-            Vcenters = json.load(response.json)
+            Vcenters = json.loads(response.json)
             checkSlots(Vcenters)
         time.sleep(6000)
 
